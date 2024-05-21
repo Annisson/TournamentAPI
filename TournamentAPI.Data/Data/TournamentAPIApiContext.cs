4f22eadcc156cd7bcf1e214cef42c0a1e82b45f7
Annica Andersson <annicaandersson@live.se>
2024-05-21 16:39:51 +0200

    Add project files.

---
+++ TournamentAPI.Data/Data/TournamentAPIApiContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TournamentAPI.Core.Entities;
+
+namespace TournamentAPI.Data.Data
+{
+    public class TournamentAPIApiContext : DbContext
+    {
+        public TournamentAPIApiContext (DbContextOptions<TournamentAPIApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Tournament> Tournament { get; set; } = default!;
+        public DbSet<Game> Game { get; set; } = default!;
+    }
+}
