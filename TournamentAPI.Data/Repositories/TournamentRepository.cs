f48b5c965f974a1a010ece2e377d2aa482fadce9
Annica Andersson <annicaandersson@live.se>
2024-05-26 18:37:05 +0200

    Add Tournament repository

---
+++ TournamentAPI.Data/Repositories/TournamentRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TournamentAPI.Core.Entities;
+using TournamentAPI.Core.Repositories;
+using TournamentAPI.Data.Data;
+
+namespace TournamentAPI.Data.Repositories
+{
+    public class TournamentRepository : ITournamentRepository
+    {
+        private readonly TournamentAPIApiContext _context;
+
+        public TournamentRepository(TournamentAPIApiContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<IEnumerable<Tournament>> GetAllAsync()
+        {
+            return await _context.Tournament.ToListAsync();
+        }
+
+        public async Task<Tournament> GetAsync(int id)
+        {
+            return await _context.Tournament.FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task<bool> AnyAsync(int id)
+        {
+            return await _context.Tournament.AnyAsync(t => t.Id == id);
+        }
+
+        public void Add(Tournament tournament)
+        {
+            _context.Tournament.Add(tournament);
+            _context.SaveChanges();
+        }
+
+        public void Update(Tournament tournament)
+        {
+            var existingEntity = _context.Tournament.Local.FirstOrDefault(entry => entry.Id == tournament.Id);
+            if (existingEntity != null)
+            {
+                _context.Entry(existingEntity).State = EntityState.Detached;
+            }
+
+            _context.Tournament.Update(tournament);
+            _context.SaveChanges();
+        }
+
+        public void Remove(Tournament tournament)
+        {
+            _context.Tournament.Remove(tournament);
+            _context.SaveChanges();
+        }
+
+    }
+}
