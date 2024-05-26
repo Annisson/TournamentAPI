d28881d7dd55b097746a7c0f7b18968043c2dbfe
Annica Andersson <annicaandersson@live.se>
2024-05-26 18:37:54 +0200

    Add Game repository

---
+++ TournamentAPI.Core/Repositories/IGameRepository.cs
@@ -0,0 +1,15 @@
+﻿using TournamentAPI.Core.Entities;
+
+namespace TournamentAPI.Core.Repositories
+{
+    public interface IGameRepository
+    {
+        Task<IEnumerable<Game>> GetAllAsync();
+        Task<Game> GetAsync(int id);
+        Task<bool> AnyAsync(int id);
+        void Add(Game game);
+        void Update(Game game);
+        void Remove(Game game);
+
+    }
+}
---
+++ TournamentAPI.Data/Repositories/GameRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TournamentAPI.Core.Entities;
+using TournamentAPI.Core.Repositories;
+using TournamentAPI.Data.Data;
+
+namespace TournamentAPI.Data.Repositories
+{
+    public class GameRepository : IGameRepository
+    {
+        private readonly TournamentAPIApiContext _context;
+
+        public GameRepository(TournamentAPIApiContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<IEnumerable<Game>> GetAllAsync()
+        {
+            return await _context.Game.ToListAsync();
+        }
+
+        public async Task<Game> GetAsync(int id)
+        {
+            return await _context.Game.FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task<bool> AnyAsync(int id)
+        {
+            return await _context.Game.AnyAsync(t => t.Id == id);
+        }
+
+        public void Add(Game game)
+        {
+            _context.Add(game);
+            _context.SaveChangesAsync();
+        }
+
+        public void Update(Game game)
+        {
+            _context.Update(game);
+            _context.SaveChangesAsync();
+        }
+
+        public void Remove(Game game)
+        {
+            _context.Remove(game);
+            _context.SaveChangesAsync();
+        }
+
+    }
+}
