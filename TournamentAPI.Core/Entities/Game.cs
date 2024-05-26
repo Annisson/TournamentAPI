34b0b7566fdc610f9363cde622a19b7eeea04685
Annica Andersson <annicaandersson@live.se>
2024-05-26 12:27:21 +0200

    Update Entities data annotations

--- TournamentAPI.Core/Entities/Game.cs
+++ TournamentAPI.Core/Entities/Game.cs
@@ -1,10 +1,17 @@
-﻿namespace TournamentAPI.Core.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TournamentAPI.Core.Entities
 {
     public class Game
     {
+        [Key]
         public int Id { get; set; }
         public string Title { get; set; }
         public DateTime Time { get; set; }
+
+        [ForeignKey("TournamentId")]
         public int TournamentId { get; set; }
+        public Tournament? Tournament { get; set; }
     }
 }
--- TournamentAPI.Core/Entities/Tournament.cs
+++ TournamentAPI.Core/Entities/Tournament.cs
@@ -1,11 +1,14 @@
-﻿namespace TournamentAPI.Core.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TournamentAPI.Core.Entities
 {
     public class Tournament
     {
+        [Key]
         public int Id { get; set; }
         public string Title { get; set; }
         public DateTime StartDate { get; set; }
-        public ICollection<Game> Games { get; set; }
+        public ICollection<Game> Games { get; set; } = new List<Game>();
 
     }
 }
