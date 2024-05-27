b6eac03e6bb494dfe8569ce6522d4ab5c1b7d175
Annica Andersson <annicaandersson@live.se>
2024-05-28 00:37:25 +0200

    Add UoW interface and class

---
+++ TournamentAPI.Data/Repositories/UnitOfWork.cs
@@ -0,0 +1,48 @@
+﻿using TournamentAPI.Core.Repositories;
+using TournamentAPI.Data.Data;
+
+namespace TournamentAPI.Data.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly TournamentAPIApiContext _context;
+        private TournamentRepository _tournamentRepository;
+        private GameRepository _gameRepository;
+
+        public UnitOfWork(TournamentAPIApiContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public ITournamentRepository TournamentRepository
+        {
+            get
+            {
+                if(_tournamentRepository == null)
+                {
+                    _tournamentRepository = new TournamentRepository(_context);
+                }
+                return _tournamentRepository;
+            }
+        }
+
+        public IGameRepository GameRepository
+        {
+            get
+            {
+
+                if (_gameRepository == null)
+                {
+                    _gameRepository = new GameRepository(_context);
+                }
+                return _gameRepository;
+            }
+        }
+
+
+        public async Task CompleteAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
